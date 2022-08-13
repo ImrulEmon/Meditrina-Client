73cc10d8cc64fe62c4c01ba19ab53ac2d2a1c5da
ImrulEmon <emimrulemon@gmail.com>
2022-08-13 11:47:36 +0600

    dependencies : daisyUi,tailwind,react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <h1 className="">
+      Hello world!
+    </h1>
+    <button class="btn w-64 rounded-full">Button</button>
+
     </div>
   );
 }
