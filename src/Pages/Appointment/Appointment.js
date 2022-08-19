46c5d7a926c78c3759c7c13175faf4b4a8abe03b
ImrulEmon <emimrulemon@gmail.com>
2022-08-19 11:17:42 +0600

    Appointment Calender

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer2 from '../../Components/Footer2/Footer2';
 import './Appointment.css';
+import AppointmentBanner from './AppointmentBanner';
 
 const Appointment = () => {
     return (
         <div>
+            <AppointmentBanner/>
             <Footer2/>
         </div>
     );
--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import bg from '../../assets/images/bg.png';
+import chair from '../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from "date-fns";
 
 const AppointmentBanner = () => {
+    const [date,setDate]=useState(new Date());
+    let footer = <p>Please pick a day.</p>;
+  if (date) {
+    footer = <p>Appointment Schedule <span className="font-bold text-primary">{format(date ,'PP')}</span>.</p>;
+  }
+  
   return (
-    <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="hero min-h-screen bg-cover bg-no-repeat bg-fixed"
+    style={{backgroundImage:`url(${bg})`}}
+    >
+      <div class="hero-content flex-col lg:flex-row-reverse gap-20">
         <img
-          src="https://placeimg.com/260/400/arch"
-          class="max-w-sm rounded-lg shadow-2xl"
+          src={chair}
+          class="max-w-sm rounded-lg shadow-2xl object-contain w-full h-full object-center lg:w-full lg:h-full "
+        alt="chair"
         />
-        <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button class="btn btn-primary">Get Started</button>
+        <div className="">
+     
+          <DayPicker 
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          footer={footer}/>
         </div>
       </div>
     </div>
