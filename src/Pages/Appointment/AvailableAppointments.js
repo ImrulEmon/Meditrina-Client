caef653e2164e2e0408e5348ee5b49ecfcb3f6a9
ImrulEmon <emimrulemon@gmail.com>
2022-08-19 17:53:51 +0600

    Appointment page modeal on clicking BookAppointment

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
 import Service from "./Service";
 
 const AvailableAppointments = ({ date }) => {
   const [services, setServices] = useState([]);
+  const [treatment,setTreatment] = useState({})
   useEffect(() => {
     fetch("services.json")
       .then((res) => res.json())
@@ -19,9 +21,13 @@ const AvailableAppointments = ({ date }) => {
       <h4 className="text-center text-[#939393]">Pleae select a service</h4>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-12 my-8 mx-auto">
         {services.map((service) => (
-          <Service key={service._id} service={service}></Service>
+          <Service key={service._id} 
+          service={service}
+          setTreatment={setTreatment}
+          ></Service>
         ))}
       </div>
+      {treatment && <BookingModal treatment={treatment}></BookingModal>}
     </div>
   );
 };
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Service = ({ service }) => {
+const Service = ({ service,setTreatment }) => {
   const { name, slots } = service;
   return (
-    <div class="card max-w-full bg-base-100 shadow-xl">
-      <div class="card-body py-12">
-        <h2 class="card-title text-secondary mx-auto">{name}</h2>
+    <div className="card max-w-full bg-base-100 shadow-xl">
+      <div className="card-body py-12">
+        <h2 className="card-title text-secondary mx-auto">{name}</h2>
         <p className="text-center">
           {slots.length > 1 ? (
             <span className="text-sm text-black">{slots[0]}</span>
@@ -16,13 +16,9 @@ const Service = ({ service }) => {
         <p className="text-center">
           {slots.length} {slots.length > 1 ? "slots" : "slot"} available
         </p>
-        <div class="card-actions justify-center py-4">
-          <button
-            disabled={slots.length === 0}
-            class="btn btn-secondary text-white font-bold"
-          >
-            Book Appointment
-          </button>
+        <div className="card-actions justify-center py-4">
+          <label htmlFor="booking-modal" className="btn modal-button btn-secondary text-white font-bold" disabled={slots.length === 0}
+            onClick={()=>setTreatment(service)}>Book Appointment</label>
         </div>
       </div>
     </div>
